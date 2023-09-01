d3fd8248e6920de09a22437e56dea3fab70a8f64
Darmawan <darmawanjr88@gmail.com>
2023-09-01 12:30:49 +0800

    added script in index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, createBrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
+import Home from './pages/Home'
+import About from './pages/About';
 import App from './App'
 import './index.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/Github-Pages-Demo",
+    element: <App />,
+    children: [
+      {
+        path: "/Github-Pages-Demo",
+        element: <Home />,
+      },
+      {
+        path: "/Github-Pages-Demo",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+
   </React.StrictMode>,
 )
